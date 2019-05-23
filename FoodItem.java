dcaafa4d1bc6986b2b429f22e7e32b9db83299dd
M4ST3RM4GIC <z0ck3r@myway.de>
2019-05-24 02:06:26 +1200

    switched to relative path when files are in the graphics package

--- FoodItem.java
+++ FoodItem.java
@@ -47,7 +47,7 @@ public class FoodItem extends Item{
 
 	 public void stringIfy(int choice) {
 		 try {
- 			FileInputStream fs= new FileInputStream("/Users/Royal/Desktop/Game/items.txt");
+ 			FileInputStream fs= new FileInputStream("/graphics/items.txt");
  		  BufferedReader br = new BufferedReader(new InputStreamReader(fs));
 
  		  for(int i = 0; i < choice; ++i)
