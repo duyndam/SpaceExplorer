67a23b045a1fcf9151c15dba8debd8c5e6246665
M4ST3RM4GIC <z0ck3r@myway.de>
2019-05-23 17:33:47 +1200

    Added random planet functions

--- Planet.java
+++ Planet.java
@@ -1,4 +1,7 @@
 // Import statements
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Random;
 /**
@@ -9,21 +12,82 @@ import java.util.Random;
 
 public class Planet {
 
+	private final int MIN_AMOUNT_MONEY = 10;
+	private final int MAX_AMOUNT_MONEY = 50;
+	private final int MAX_DIFFERENT_PARTS_COUNT = 24;
+	static private int MAX_PLANET_COUNT = 10;
+	private boolean partFound = false;
 	static private final String[] PLANET_NAMES = {	"kolvupra", "dotruinia", "batruna",
 													"xagnoth", "seutis", "uebos",
 													"gidazuno", "moxuphun",	"dippe PJ06", 
 													"trars 3" };
-	Random randomGenerator = null;
+	private Random randomGenerator = null;
 	public Planet()
 	{
 		randomGenerator = new Random();
 	}
 
-  public String toString() {
-    String body = "";
-    body += "The planet " + this.name + " is a(n) " + this.climate + " planet.";
-    //body += "\n You find a(n) " + this.found + ".";
-    return body;
-  }
+	public MedicalItem get_RandomMedicalItem()
+	{
+		MedicalItem generatedItem = new MedicalItem();
+		int iChoice = 0;
+		iChoice = randomGenerator.nextInt(MedicalItem.MEDIC_INDEX_MIN + MedicalItem.AMOUNT_MED_ITEMS - MedicalItem.MEDIC_INDEX_MIN +1) + MedicalItem.MEDIC_INDEX_MIN;
+		generatedItem.stringIfy(iChoice);
+		return generatedItem;
+	}
+	
+	public FoodItem get_RandomFoodItem()
+	{
+		FoodItem generatedItem = new FoodItem();
+		int iChoice = 0;
+		iChoice = randomGenerator.nextInt(FoodItem.AMOUNT_FOOD_ITEMS);
+		generatedItem.stringIfy(iChoice);
+		return generatedItem;
+	}
+	
+	public String get_RandomShipPart()
+	{
+		String strRandomShipPart = "";
+		int iChoice = 0;
+		iChoice = randomGenerator.nextInt(MAX_DIFFERENT_PARTS_COUNT);
+		try 
+		{
+			
+ 			FileInputStream fs= new FileInputStream("txts/parts.txt");
+ 			BufferedReader br = new BufferedReader(new InputStreamReader(fs));
+ 			String[] strLineSplit = null;
+ 			for (String line = br.readLine(); line != null; line = br.readLine()) {
+ 				strLineSplit = line.split(",");
+ 			    if(Integer.toString(iChoice) == strLineSplit[0])
+ 			    {
+ 			    	strRandomShipPart = strLineSplit[1];
+ 			    }
+ 			}
+		}catch(Exception e)
+		{
+			
+		}
+		return strRandomShipPart;
+	}
+	
+	public int get_RandomAmountMoney()
+	{
+		int iMoney = 0;
+		iMoney = randomGenerator.nextInt(MAX_AMOUNT_MONEY - MIN_AMOUNT_MONEY + 1) + MIN_AMOUNT_MONEY;
+		return iMoney;
+	}
+	
+	public boolean findingPart()
+	{
+		if(!partFound)
+		{
+			partFound = true;
+			return true;
+		}
+		else
+		{
+			return false;
+		}
+	}
 
 }
